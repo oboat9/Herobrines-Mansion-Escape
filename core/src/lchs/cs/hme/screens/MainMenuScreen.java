bc5023600f18e56dcbc93fbc384b5f581d61e5d3
oboat9 <ombstevenson@gmail.com>
2020-04-22 21:43:55 -0600

    changed clear color

--- core/src/lchs/cs/hme/screens/MainMenuScreen.java
+++ core/src/lchs/cs/hme/screens/MainMenuScreen.java
@@ -44,7 +44,7 @@ public class MainMenuScreen implements Screen {
 	public void render(float delta) {
 		
 		
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, .1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		
