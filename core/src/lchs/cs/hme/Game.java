d4301a69970b5f075782483dc16611a641ae378e
oboat9 <ombstevenson@gmail.com>
2020-04-22 20:04:55 -0600

    added basic movement

--- core/src/lchs/cs/hme/Game.java
+++ core/src/lchs/cs/hme/Game.java
@@ -2,6 +2,7 @@ package lchs.cs.hme;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x = 0;
+	float y = 0;
 	
 	@Override
 	public void create () {
@@ -20,8 +23,23 @@ public class Game extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		//character movement
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			y += 5;
+		} if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y -= 5;
+		} if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 5;
+		} if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 5;
+		}
+		
+		// start drawing images to the screen
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		//draw the img image
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
