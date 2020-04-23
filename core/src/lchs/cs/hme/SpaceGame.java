c82141f5b4bf6f1cdb05103334a4fd5e0ea28b07
oboat9 <ombstevenson@gmail.com>
2020-04-22 20:27:52 -0600

    moved main game code into its own file (cleaned up the main class)

--- core/src/lchs/cs/hme/SpaceGame.java
+++ core/src/lchs/cs/hme/SpaceGame.java
@@ -1,57 +1,30 @@
 package lchs.cs.hme;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import lchs.cs.hme.screens.MainGameScreen;
 
 public class SpaceGame extends Game {
 	
-	public static final float SPEED = 120;
+	public SpriteBatch batch;
 	
-	SpriteBatch batch;
-	Texture img;
-	float x = 0;
-	float y = 0;
 	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new MainGameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//System.out.println(Gdx.graphics.getDeltaTime());
-		
-		//character movement
-		if(Gdx.input.isKeyPressed(Keys.UP)) {
-			y += SPEED*Gdx.graphics.getDeltaTime();
-		} if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			y -= SPEED*Gdx.graphics.getDeltaTime();
-		} if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			x += SPEED*Gdx.graphics.getDeltaTime();
-		} if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			x -= SPEED*Gdx.graphics.getDeltaTime();
-		}
-		
-		// start drawing images to the screen
-		batch.begin();
-		
-		//draw the img image
-		batch.draw(img, x, y);
-		batch.end();
+		super.render();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
