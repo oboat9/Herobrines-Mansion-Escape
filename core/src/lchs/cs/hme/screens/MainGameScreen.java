5eee83cdfe014670093d6a3d28360fa465053816
oboat9 <ombstevenson@gmail.com>
2020-05-04 13:51:30 -0600

    Update MainGameScreen.java

--- core/src/lchs/cs/hme/screens/MainGameScreen.java
+++ core/src/lchs/cs/hme/screens/MainGameScreen.java
@@ -5,25 +5,48 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.Animation;
+//import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import lchs.cs.hme.SpaceGame;
 
 public class MainGameScreen implements Screen{
 
-	public static final float SPEED = 240;
-	Texture playerImg;
+	public static final float SPEED = 300;
+	public static final float SHIP_ANIMATION_SPEED = 0.5f;
+	public static final int SHIP_WIDTH_PIXEL = 32;
+	public static final int SHIP_HEIGHT_PIXEL = 72;
+	
+	public static final int SHIPWIDTH = SHIP_WIDTH_PIXEL * 3;
+	public static final int SHIPHEIGHT = SHIP_HEIGHT_PIXEL * 3;
+
+	//Animation<TextureRegion>[] rolls;
+	
 	float x = 0;
 	float y = 0;
+	
+	
+	
+	int roll;
+	
+	float stateTime; 
+	
+	Texture playerImg;
 	
 	SpaceGame game;
 	
 	public MainGameScreen (SpaceGame game) {
 		this.game = game;
+		y = 15;
+		x = SpaceGame.WIDTH /2 - SHIPWIDTH /2;
+		
+		
+
 	}
 	
 	@Override
 	public void show() {
-		playerImg = new Texture("Steve.png");
+		playerImg = new Texture ("Steve.png");
 		
 	}
 
@@ -47,13 +70,15 @@ public class MainGameScreen implements Screen{
 			game.setScreen(new MainMenuScreen(game));
 		}
 		
+		stateTime += delta;
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		// start drawing images to the screen
 		game.batch.begin();
 		//draw the img image
-		game.batch.draw(playerImg, x, y, 128, 288);
+		game.batch.draw(playerImg, x, y, SHIPWIDTH, SHIPHEIGHT);
 		// stop drawing things to the screen
 		game.batch.end();
 		
@@ -85,7 +110,7 @@ public class MainGameScreen implements Screen{
 
 	@Override
 	public void dispose() {
-		playerImg.dispose();
+		//playerImg.dispose();
 		
 	}
 
