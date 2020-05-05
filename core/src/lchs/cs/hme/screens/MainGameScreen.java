3017681479ebdb277492fdc73adeaaabecf02672
oboat9 <ombstevenson@gmail.com>
2020-05-05 15:56:38 -0600

    organized some imports

--- core/src/lchs/cs/hme/screens/MainGameScreen.java
+++ core/src/lchs/cs/hme/screens/MainGameScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 import lchs.cs.hme.HerobrineEscape;
 import lchs.cs.hme.tools.TextInput;
