d99db01a2ce68e2432684aaaf0a5707b7302d61e
oboat9 <ombstevenson@gmail.com>
2020-05-05 17:24:43 -0600

    more comments

--- desktop/src/lchs/cs/hme/desktop/DesktopLauncher.java
+++ desktop/src/lchs/cs/hme/desktop/DesktopLauncher.java
@@ -8,11 +8,15 @@ import lchs.cs.hme.HerobrineEscape;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		// sets various settings for the window
 		config.foregroundFPS = 60;
 		config.width = HerobrineEscape.WIDTH;
 		config.height = HerobrineEscape.HEIGHT;
 		config.resizable = false;
 		config.fullscreen = false;
+		
+		// add the window icons
 		config.addIcon("images/icons/icon_128.png", FileType.Internal);
 		config.addIcon("images/icons/icon_32.png", FileType.Internal);
 		config.addIcon("images/icons/icon_16.png", FileType.Internal);
